8fa8610288ee9802d4edb70f89ef0b8ca23fd581
Fraser Tooth <frasertooth@gmail.com>
2020-01-23 12:48:39 +0900

    Dotenv tweak for prod

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: './.env' })
+if (process.env.NODE_ENV !== 'production')
+  require('dotenv').config({ path: './.env' })
 
 const DATABASE_URL = process.env.DATABASE_URL
 
